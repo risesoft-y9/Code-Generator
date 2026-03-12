6b18c3b87fcd032fd28b5cf4a8c2aaaadfe939c9
shidaobang <shidaobang@risesoft.net>
2026-03-12 15:54:30 +0800

    feat: 自定义 springboot 应用的 banner，打印一些有用信息

--- risenet-y9boot-server-code/src/main/java/net/risesoft/CodeApplication.java
+++ risenet-y9boot-server-code/src/main/java/net/risesoft/CodeApplication.java
@@ -3,9 +3,13 @@ package net.risesoft;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import net.risesoft.y9.spring.boot.Y9Banner;
+
 @SpringBootApplication
 public class CodeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CodeApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(CodeApplication.class);
+        springApplication.setBanner(new Y9Banner());
+        springApplication.run(args);
     }
 }
